59da0d8f8c8554c4420c065963f9326ee84e6ebb
Derek Houck <derek@derekhouck.com>
2018-12-17 11:39:08 -0800

    Create GET notes list endpoint

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 
 const data = require('./db/notes');
 
 const app = express();
 
-// ADD STATIC SERVER HERE
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
 
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
