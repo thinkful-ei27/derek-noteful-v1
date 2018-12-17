3035fb5db89e956ab8ac17760fe81d72a96f66c6
Derek Houck <derek@derekhouck.com>
2018-12-17 11:47:03 -0800

    Create GET note details endpoint

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const item = data.find(item => item.id === Number(req.params.id));
+  res.json(item);
+});
+
 app.listen(8080, function() {
+  // eslint-disable-next-line no-console
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
+  // eslint-disable-next-line no-console
   console.error(err);
 });
