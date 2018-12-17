6a6ba7e466acdcc1c98d6e5c9e3ff8636b4a5b8d
Derek Houck <derek@derekhouck.com>
2018-12-17 11:33:57 -0800

    Create Express App and setup a Static Server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 'use strict';
 
-// Load array of notes
+// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
+
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
