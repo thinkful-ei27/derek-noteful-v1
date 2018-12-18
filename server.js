aeed134a711d6c29e7cd76da589fd39e4d08e462
Derek Houck <derek@derekhouck.com>
2018-12-18 11:26:33 -0800

    Express Error-Handling Middleware

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(requestLogger);
 
 app.use(express.static('public'));
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
 app.get('/api/notes', (req, res) => {
 
   const { searchTerm } = req.query;
@@ -25,6 +29,20 @@ app.get('/api/notes', (req, res) => {
 app.get('/api/notes/:id', (req, res) => {
   const item = data.find(item => item.id === Number(req.params.id));
   res.json(item);
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.listen(PORT, function() {
