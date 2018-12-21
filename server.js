3eae256f7b0f8c25c4bdbbec4418d75a2ef0b34b
Derek Houck <derek@derekhouck.com>
2018-12-21 11:14:09 -0800

    Update server.js for testing

--- server.js
+++ server.js
@@ -33,10 +33,15 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(PORT, function() {
-  // eslint-disable-next-line no-console
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  // eslint-disable-next-line no-console
-  console.error(err);
-});
+// Listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function() {
+    // eslint-disable-next-line no-console
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
+}
+
+module.exports = app; // Export for testing
